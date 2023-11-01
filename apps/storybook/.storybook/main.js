f20970788c06341d7847764ffd543fe93e5e34e1
José Lugo <chepe.time@gmail.com>
2023-10-31 18:34:51 -0600

    Better vscode settings

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -18,6 +18,10 @@ const config = {
     getAbsolutePath("@storybook/addon-links"),
     getAbsolutePath("@storybook/addon-essentials"),
     getAbsolutePath("@storybook/addon-interactions"),
+    getAbsolutePath("@storybook/addon-styling-webpack"),
+    getAbsolutePath("@storybook/addon-designs"),
+    getAbsolutePath("@storybook/addon-themes"),
+    getAbsolutePath("@storybook/addon-a11y"),
   ],
   framework: {
     name: getAbsolutePath("@storybook/html-vite"),
